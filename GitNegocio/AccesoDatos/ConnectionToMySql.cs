f90a94fc87cf5658c54a4b05983b84172a2a836a
Dbenitez <diegobenitez@live.com.mx>
2020-06-24 17:22:32 -0500

    yeah

---
+++ GitNegocio/AccesoDatos/ConnectionToMySql.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    class ConnectionToMySql
+    {
+        private readonly string connectionString;
+
+        public ConnectionToMySql()
+        {
+            connectionString = "Server=localhost; DataBase=negocio; User=root; port=3306; password=;";
+        }
+
+        protected MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
